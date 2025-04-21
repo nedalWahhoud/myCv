9ca0015a9823ab4547cd4a048a54a0cb3bd8ea43
nedalWahhoud <nedal.1992.29@gmail.com>
2025-04-21 18:28:08 +0200

    update

--- staticVariable.php
+++ staticVariable.php
@@ -4,8 +4,8 @@
     public static string $myCvNamePage = "myCvPage.php";
     public static string $contactPage = "contact.php";
     public static string $cvPdfId = "4";
-    public static string $receiverEmail = "nedal.1992.29@gmail.com";
-    public static string $passwordEmail = "vesu cjfp zlzn xjqc";
+    public static string $receiverEmail = "";
+    public static string $passwordEmail = "";
     public static string $linkedinLink = 'https://www.linkedin.com/in/nedal-wahhoud-81b26816a/';
     public static string $xingLink = 'https://www.xing.com/profile/Nedal_Wahhoud/web_profiles/';
     public static string $instgramLink = "https://www.instagram.com/nedal_wahhoud/";
