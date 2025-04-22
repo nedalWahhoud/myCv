9ca71d48a6147c96c39dc9663810fdbe4bc39ffa
nedalWahhoud <nedal.1992.29@gmail.com>
2025-04-22 11:27:44 +0200

    mysql update

--- mycv.sql
+++ mycv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 21. Apr 2025 um 15:18
+-- Erstellungszeit: 22. Apr 2025 um 11:26
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -54,7 +54,7 @@ INSERT INTO `data` (`id`, `data_en`, `data_de`) VALUES
 
 CREATE TABLE `experience` (
   `id` int(11) NOT NULL,
-  `key_word` varchar(10) DEFAULT NULL,
+  `key_word` varchar(20) NOT NULL,
   `TiLevel` int(11) NOT NULL,
   `date` varchar(20) DEFAULT NULL,
   `kW_des` varchar(11) DEFAULT NULL
@@ -99,7 +99,7 @@ INSERT INTO `experience` (`id`, `key_word`, `TiLevel`, `date`, `kW_des`) VALUES
 
 CREATE TABLE `skills` (
   `id` int(11) NOT NULL,
-  `key_word` varchar(13) NOT NULL
+  `key_word` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -129,9 +129,9 @@ INSERT INTO `skills` (`id`, `key_word`) VALUES
 CREATE TABLE `translation` (
   `id` int(3) NOT NULL,
   `key_word` varchar(20) NOT NULL,
-  `en` varchar(150) NOT NULL,
-  `de` varchar(150) NOT NULL,
-  `data_id` int(11) DEFAULT NULL
+  `en` varchar(255) NOT NULL,
+  `de` varchar(255) NOT NULL,
+  `data_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -139,72 +139,72 @@ CREATE TABLE `translation` (
 --
 
 INSERT INTO `translation` (`id`, `key_word`, `en`, `de`, `data_id`) VALUES
-(1, 'home', 'Home', 'Startseite', NULL),
-(2, 'CV', 'My CV', 'Lebenslauf', NULL),
-(3, 'contact', 'Contact', 'Kontakt', NULL),
+(1, 'home', 'Home', 'Startseite', 0),
+(2, 'CV', 'My CV', 'Lebenslauf', 0),
+(3, 'contact', 'Contact', 'Kontakt', 0),
 (4, 'lang', 'Language', 'Sprache', 1),
 (5, 'myName', 'Mohamad Nedal', 'Wahhoud', 2),
-(6, 'JobTitle', 'IT Specialist for Application Development', 'Fachinformatiker Anwendungsentwicklung', NULL),
-(7, 'cardText0', 'Who I am & what I do', 'Wer ich bin & was ich mache', NULL),
-(8, 'cardText2', 'after completing my study as an IT specialist in application development in 2014', 'nach Abschluss meines Studiums als Fachinformatiker für Anwendungsentwicklung im Jahr 2014 ', NULL),
-(9, 'hello', 'Hello', 'Hallo', NULL),
-(10, 'cardText1', 'My name is Mohammad Nedal Wahhoud, 33 years old, I am married,', 'Mein Name ist Mohammad Nedal Wahhoud, 33 Jahre alt, ich bin verheiratet,', NULL),
-(11, 'cardText3', 'I travelled from Syria to Germany,', 'bin ich von Syrien nach Deutschland gereist,', NULL),
-(12, 'cardText4', 'then I took part in various integration courses,', 'dann habe ich an verschiedenen Integrationskursen teilgenommen, ', NULL),
-(13, 'cardText5', 'to integrate better into society and improve my German skills.', ', um mich besser in die Gesellschaft einzuleben und meine Deutschkenntnisse zu verbessern. ', NULL),
-(14, 'cardText6', 'Afterwards I did an internship at Multivac Sepp Haggenmüller Se & Co. Kg,', 'danach habe ich ein Praktikum bei Multivac Sepp Haggenmüller Se & Co. Kg gemacht,', NULL),
-(15, 'cardText7', 'I then worked for several years as a software development assistant at IFM Prover GmbH.', 'Anschließend war ich 7 Jahre als Software-Entwicklungsassistent bei IFM Prover GmbH tätig. ', NULL),
-(16, 'la_name', 'Englisch', 'Deutsch', NULL),
-(17, 'la_code', 'EN', 'DE', NULL),
-(18, 'aboMe', 'About me', 'Zu meiner Person', NULL),
+(6, 'JobTitle', 'IT Specialist for Application Development', 'Fachinformatiker Anwendungsentwicklung', 0),
+(7, 'cardText0', 'Who I am & what I do', 'Wer ich bin & was ich mache', 0),
+(8, 'cardText2', 'after completing my study as an IT specialist in application development in 2014', 'nach Abschluss meines Studiums als Fachinformatiker für Anwendungsentwicklung im Jahr 2014 ', 0),
+(9, 'hello', 'Hello', 'Hallo', 0),
+(10, 'cardText1', 'My name is Mohammad Nedal Wahhoud, 33 years old, I am married,', 'Mein Name ist Mohammad Nedal Wahhoud, 33 Jahre alt, ich bin verheiratet,', 0),
+(11, 'cardText3', 'I travelled from Syria to Germany,', 'bin ich von Syrien nach Deutschland gereist,', 0),
+(12, 'cardText4', 'then I took part in various integration courses,', 'dann habe ich an verschiedenen Integrationskursen teilgenommen, ', 0),
+(13, 'cardText5', 'to integrate better into society and improve my German skills.', ', um mich besser in die Gesellschaft einzuleben und meine Deutschkenntnisse zu verbessern. ', 0),
+(14, 'cardText6', 'Afterwards I did an internship at Multivac Sepp Haggenmüller Se & Co. Kg,', 'danach habe ich ein Praktikum bei Multivac Sepp Haggenmüller Se & Co. Kg gemacht,', 0),
+(15, 'cardText7', 'I then worked for several years as a software development assistant at IFM Prover GmbH.', 'Anschließend war ich 7 Jahre als Software-Entwicklungsassistent bei IFM Prover GmbH tätig. ', 0),
+(16, 'la_name', 'Englisch', 'Deutsch', 0),
+(17, 'la_code', 'EN', 'DE', 0),
+(18, 'aboMe', 'About me', 'Zu meiner Person', 0),
 (19, 'DlCv', 'Download CV', 'Lebenslauf herunterladen', 4),
-(20, 'born', 'Born on January 29, 1992 in Homs/Syria', 'Geboren am 29.01.1992 in Homs/Syrien', NULL),
-(21, 'nationalit', 'Nationality: German/Syrian', 'Nationalität: Deutsch/Syrisch', NULL),
-(22, 'residence', 'Place of residence: 87700 DE-Memmingen ', 'Wohnort: 87700 DE-Memmingen', NULL),
-(23, 'Number', 'Number', 'Nummer', NULL),
-(24, 'skills', 'Skills', 'Fähigkeiten', NULL),
-(25, 'skillsText0', 'Extensive experience in various programming languages, especially C#.', 'Vielfältige Erfahrung in verschiedenen Programmiersprachen, insbesondere in C#.  ', NULL),
-(26, 'skillsText1', 'Extensive experience in web design and web development using PHP, JavaScript, CSS, HTML, and Velocity.', 'Umfangreiche Erfahrung im Webdesign und in der Webentwicklung mit PHP, JavaScript, CSS, HTML und Velocity.', NULL),
-(27, 'skillsText2', 'Good knowledge of database design, management, and querying (SQL, MySQL).', 'Gute Kenntnisse Kenntnisse in Datenbankdesign, in der Verwaltung und Abfrage (SQL, MySQL).', NULL),
-(29, 'skillsText3', 'In-depth knowledge of Visual Studio and Visual Studio Code.', 'Fundierte Kenntnisse in Visual Studio, Visual Studio Code.', NULL),
-(30, 'skillsText4', 'Good experience with Git and GitHub.', 'Gute Erfahrung in Git und Github.', NULL),
-(31, 'skillsText5', 'Good hardware knowledge.', 'Gute Hardwarekenntnisse.', NULL),
-(32, 'skillsText6', 'Extensive IT background.', 'Umfassende IT-Hintergrund.', NULL),
-(33, 'skillsText7', 'Good knowledge of networking.', 'Gute Netzwerk-Kenntnisse.', NULL),
-(34, 'skillsText8', 'Basic knowledge of electronics and electrical engineering.', 'Grundkenntnisse in Elektronik und Elektrotechnik.', NULL),
-(35, 'skillsText9', 'In-depth knowledge of the Windows operating system.', 'Fundierte in Windows-Betriebssystem.', NULL),
-(36, 'ifm', 'Software Development Assistant at Ifm prover GmbH Tettnang', 'Software-Entwicklungsassistent bei Ifm prover GmbH Tettnang', NULL),
-(38, 'textIfm0', 'Development, implementation, and maintenance of software applications and tools using C# to meet specific project requirements.', 'Entwicklung, Implementierung und Wartung von Softwareanwendungen und Tools unter Verwendung von C# um spezifische Projektanforderungen zu erfüllen.', NULL),
-(39, 'textIfm1', 'Implementation of defined features for internal development tools.', 'Umsetzung von definierten Features für entwicklungsinterne Tools.', NULL),
-(40, 'textIfm2', 'Conducting development-related tests and documenting the results.', 'Durchführung von entwicklungsbegleitenden Tests und Dokumentation der Ergebnisse', NULL),
-(41, 'textIfm3', 'Error analysis and troubleshooting in developed tools.', 'Fehleranalyse und -behebung in entwickelten Tools.', NULL),
-(42, 'textIfm4', 'Developing and maintaining internal websites using HTML, PHP, Velocity, JS, CSS, SQL (Frontend and Backend).', 'Interne Webseiten unter Verwendung von HTML, PHP, Velocity, JS, CSS, SQL, (Front und Backend) entwickeln und pflegen.', NULL),
-(43, 'textIfm5', 'Performing various laboratory tasks, including (e.g., sample analysis, experiments, documentation).', 'Durchführung verschiedener Laboraufgaben, einschließlich (z. B. Probenanalyse, Experi-mente, Dokumentation).', NULL),
-(44, 'textIfm6', 'Use, configure, and manage Polarion.', 'Polarion verwenden, konfigurieren und verwalten.', NULL),
-(45, 'trialIfm', 'Trial work at IFM, Tettnang', 'Probearbeiten bei IFM, Tettnang ', NULL),
-(46, 'JobSe', 'Job seeker', 'Arbeitsuchend', NULL),
-(47, 'multivac ', 'Internship at: Multivac in Wolfertschwenden', 'Praktikum bei: Multivac in Wolfertschwenden', NULL),
-(48, 'EnGermany', 'Entry into Germany, integration courses', 'Einreise nach Deutschland, Integrationskursen', NULL),
-(49, 'WExperienc', 'Work Experience', 'Berufserfahrung', NULL),
-(50, 'education', 'Education', 'Ausbildung', NULL),
-(51, 'CTech', 'Computer technician specializing in software development (recognized in Germany as an IT specialist specializing in application development)', 'Computer-Techniker Fachrichtung Softwareentwicklung (In Deutschland anerkannt als Fachinformatiker Fachrichtung Anwendungsentwicklung)\r\n', NULL),
-(52, 'HiSchool', 'High School Diploma', 'Abitur', NULL),
-(53, 'hS', 'in Homs/Syria', 'in Homs/Syrien', NULL),
-(54, 'langSkills', 'Language skills', 'Sprachkenntnisse', NULL),
-(55, 'deSkill', 'German: fluent in Spoken and Written (B2 level with certificate)', 'Deutsch: verhandlungssicher in Wort und Schrift (B2 Niveau mit Zertifikat) ', NULL),
-(56, 'enSkill', 'English: Good skills in reading and writing', 'Englisch: Gute Kenntnisse in Lesen, Schreiben', NULL),
-(57, 'arSkill', 'Arabic: native language', 'Arabisch: Muttersprache', NULL),
-(58, 'privWork', 'Insights into my private Work', 'Einblicke in meine private Arbeit', NULL),
+(20, 'born', 'Born on January 29, 1992 in Homs/Syria', 'Geboren am 29.01.1992 in Homs/Syrien', 0),
+(21, 'nationalit', 'Nationality: German/Syrian', 'Nationalität: Deutsch/Syrisch', 0),
+(22, 'residence', 'Place of residence: 87700 DE-Memmingen ', 'Wohnort: 87700 DE-Memmingen', 0),
+(23, 'Number', 'Number', 'Nummer', 0),
+(24, 'skills', 'Skills', 'Fähigkeiten', 0),
+(25, 'skillsText0', 'Extensive experience in various programming languages, especially C#.', 'Vielfältige Erfahrung in verschiedenen Programmiersprachen, insbesondere in C#.  ', 0),
+(26, 'skillsText1', 'Extensive experience in web design and web development using PHP, JavaScript, CSS, HTML, and Velocity.', 'Umfangreiche Erfahrung im Webdesign und in der Webentwicklung mit PHP, JavaScript, CSS, HTML und Velocity.', 0),
+(27, 'skillsText2', 'Good knowledge of database design, management, and querying (SQL, MySQL).', 'Gute Kenntnisse Kenntnisse in Datenbankdesign, in der Verwaltung und Abfrage (SQL, MySQL).', 0),
+(29, 'skillsText3', 'In-depth knowledge of Visual Studio and Visual Studio Code.', 'Fundierte Kenntnisse in Visual Studio, Visual Studio Code.', 0),
+(30, 'skillsText4', 'Good experience with Git and GitHub.', 'Gute Erfahrung in Git und Github.', 0),
+(31, 'skillsText5', 'Good hardware knowledge.', 'Gute Hardwarekenntnisse.', 0),
+(32, 'skillsText6', 'Extensive IT background.', 'Umfassende IT-Hintergrund.', 0),
+(33, 'skillsText7', 'Good knowledge of networking.', 'Gute Netzwerk-Kenntnisse.', 0),
+(34, 'skillsText8', 'Basic knowledge of electronics and electrical engineering.', 'Grundkenntnisse in Elektronik und Elektrotechnik.', 0),
+(35, 'skillsText9', 'In-depth knowledge of the Windows operating system.', 'Fundierte in Windows-Betriebssystem.', 0),
+(36, 'ifm', 'Software Development Assistant at Ifm prover GmbH Tettnang', 'Software-Entwicklungsassistent bei Ifm prover GmbH Tettnang', 0),
+(38, 'textIfm0', 'Development, implementation and maintenance of software applications and tools using C# to meet specific project requirements as well as documentation of the developed software.', 'Entwicklung, Implementierung und Wartung von Softwareanwendungen und Tools unter Verwendung von C# um spezifische Projektanforderungen zu erfüllen sowie Dokumentati-on der entwickelten Software.', 0),
+(39, 'textIfm1', 'Implementation of defined features for internal development tools.', 'Umsetzung von definierten Features für entwicklungsinterne Tools.', 0),
+(40, 'textIfm2', 'Conducting development-related tests and documenting the results.', 'Durchführung von entwicklungsbegleitenden Tests und Dokumentation der Ergebnisse', 0),
+(41, 'textIfm3', 'Error analysis and troubleshooting in developed tools.', 'Fehleranalyse und -behebung in entwickelten Tools.', 0),
+(42, 'textIfm4', 'Developing and maintaining internal websites using HTML, PHP, Velocity, JS, CSS, SQL (Frontend and Backend).', 'Interne Webseiten unter Verwendung von HTML, PHP, Velocity, JS, CSS, SQL, (Front und Backend) entwickeln und pflegen.', 0),
+(43, 'textIfm5', 'Performing various laboratory tasks, including (e.g., sample analysis, experiments, documentation).', 'Durchführung verschiedener Laboraufgaben, einschließlich (z. B. Probenanalyse, Experi-mente, Dokumentation).', 0),
+(44, 'textIfm6', 'Use, configure, and manage Polarion.', 'Polarion verwenden, konfigurieren und verwalten.', 0),
+(45, 'trialIfm', 'Trial work at IFM, Tettnang', 'Probearbeiten bei IFM, Tettnang ', 0),
+(46, 'JobSe', 'Job seeker', 'Arbeitsuchend', 0),
+(47, 'multivac ', 'Internship at: Multivac in Wolfertschwenden', 'Praktikum bei: Multivac in Wolfertschwenden', 0),
+(48, 'EnGermany', 'Entry into Germany, integration courses', 'Einreise nach Deutschland, Integrationskursen', 0),
+(49, 'WExperienc', 'Work Experience', 'Berufserfahrung', 0),
+(50, 'education', 'Education', 'Ausbildung', 0),
+(51, 'CTech', 'Computer technician specializing in software development (recognized in Germany as an IT specialist specializing in application development)', 'Computer-Techniker Fachrichtung Softwareentwicklung (In Deutschland anerkannt als Fachinformatiker Fachrichtung Anwendungsentwicklung)\r\n', 0),
+(52, 'HiSchool', 'High School Diploma', 'Abitur', 0),
+(53, 'hS', 'in Homs/Syria', 'in Homs/Syrien', 0),
+(54, 'langSkills', 'Language skills', 'Sprachkenntnisse', 0),
+(55, 'deSkill', 'German: fluent in Spoken and Written (B2 level with certificate)', 'Deutsch: verhandlungssicher in Wort und Schrift (B2 Niveau mit Zertifikat) ', 0),
+(56, 'enSkill', 'English: Good skills in reading and writing', 'Englisch: Gute Kenntnisse in Lesen, Schreiben', 0),
+(57, 'arSkill', 'Arabic: native language', 'Arabisch: Muttersprache', 0),
+(58, 'privWork', 'Insights into my private Work', 'Einblicke in meine private Arbeit', 0),
 (59, 'githubLink', 'https://github.com/nedalWahhoud', 'https://github.com/nedalWahhoud', 3),
-(60, 'other', 'Other', 'Sonstiges', NULL),
-(61, 'license', 'Class B driving license, own car available', 'Führerschein Klasse B, eigenes Auto vorhanden', NULL),
-(63, 'firstName', 'First Name', 'Vorname', NULL),
-(64, 'lastName', 'Last Name', 'Nachname', NULL),
-(65, 'email', 'E-Mail', 'E-Mail', NULL),
-(66, 'subject', 'Subject', 'Betreff', NULL),
-(67, 'message', 'Message', 'Nachricht', NULL),
-(68, 'send', 'Send', 'Absenden', NULL),
-(69, 'skillsText10', 'Good knowledge of Microsoft MS Office products.', 'Gute Kenntnisse in Microsoft MS-Office Produkte.', NULL);
+(60, 'other', 'Other', 'Sonstiges', 0),
+(61, 'license', 'Class B driving license, own car available', 'Führerschein Klasse B, eigenes Auto vorhanden', 0),
+(63, 'firstName', 'First Name', 'Vorname', 0),
+(64, 'lastName', 'Last Name', 'Nachname', 0),
+(65, 'email', 'E-Mail', 'E-Mail', 0),
+(66, 'subject', 'Subject', 'Betreff', 0),
+(67, 'message', 'Message', 'Nachricht', 0),
+(68, 'send', 'Send', 'Absenden', 0),
+(69, 'skillsText10', 'Good knowledge of Microsoft MS Office products.', 'Gute Kenntnisse in Microsoft MS-Office Produkte.', 0);
 
 --
 -- Indizes der exportierten Tabellen
